d6813a12c8e7128cb6589d0d1e8b0af739ed94d9
dew365dew <65011212231@msu.ac.th>
2026-05-18 14:18:07 +0700

    แก้ port

--- index.js
+++ index.js
@@ -229,6 +229,8 @@ app.get('/messages/:match_id', async (req, res) => {
 // ==========================
 // 🚀 START
 // ==========================
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000')
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
